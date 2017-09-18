484898320ebde92791b972d4099a815836547835
Esteban Rojas <estered9112@gmail.com>
2017-09-18 15:30:39 -0400

    Added footer icons with links

--- src/Footer.js
+++ src/Footer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import tmdb from '../public/images/tmdb-logo.svg';
 
 class Footer extends Component {
 
@@ -7,10 +8,10 @@ class Footer extends Component {
         return (
             <div className="ui inverted vertical footer segment">
                 <div className="ui container">
-                    <div className="ui stackable inverted divided equal height center aligned stackable grid">
-                        <div className="three wide column">github link</div>
-                        <div className="three wide column">linkedin link</div>
-                        <div className="three wide column">Powered by tmdb</div>
+                    <div className="ui stackable inverted divided equal height middle aligned center aligned stackable grid">
+                        <div className="three wide column"><a href="https://github.com/estebanred" target="_blank" className="footer-social"><i className="big github icon"></i></a></div>
+                        <div className="three wide column"><a href="https://www.linkedin.com/in/esteban-rojas-4401a286" target="_blank" className="footer-social"><i className="big linkedin square icon"></i></a></div>
+                        <div className="three wide column"><img className="tmdb-logo" src={tmdb} alt="tmdb" /></div>
                     </div>
                 </div>
             </div>
